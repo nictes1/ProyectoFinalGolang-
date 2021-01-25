ab88089e95dcc5e3601817ae6294d6fcc073640b
nicolas <nikolastesone@gmail.com>
2021-01-25 12:21:04 -0300

    Inserto tweet

---
+++ bd/insertoTweet.go
@@ -0,0 +1,31 @@
+package bd
+
+import (
+	"context"
+	"time"
+
+	"github.com/nictes1/FinalProjectGO/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+/*InsertoTweet graba el tweet en la BD*/
+func InsertoTweet(t models.GraboTweet) (string, bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	db := MongoCN.Database("microblog")
+	col := db.Collection("tweet")
+
+	registro := bson.M{
+		"userid":  t.UserID,
+		"mensaje": t.Mensaje,
+		"fecha":   t.Fecha,
+	}
+	result, err := col.InsertOne(ctx, registro)
+	if err != nil {
+		return "", false, err
+	}
+	ObjID, _ := result.InsertedID.(primitive.ObjectID)
+	return ObjID.String(), true, nil
+}
